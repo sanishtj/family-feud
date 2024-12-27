f09ce2e3389269b06fa010283b0d019e355f54a5
Sanish Joseph <sanyjose85@gmail.com>
2024-12-26 22:55:30 -0800

    logo

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -2,5 +2,17 @@ import "../game.css";
 import GameBoard from "./components/GameBoard";
 
 export default function App() {
-  return <GameBoard />;
+  return (
+    <>
+      <div className="header">
+        <div className="tilted-text">Sanish's</div>
+        <img
+          src="/Family-Feud-Logo.png"
+          alt="Family Feud Logo"
+          className="tilted-logo"
+        />
+      </div>
+      <GameBoard />
+    </>
+  );
 }
